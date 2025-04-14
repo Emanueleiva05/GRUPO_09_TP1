555987af8fbcf50cd85669d3fe68c4e1e9f3f8c7
Irineo Hiriart <irineohiriart@alu.frlp.utn.edu.ar>
2025-04-13 22:47:52 -0300

    Register - Estructura basica.

--- app/src/main/java/com/example/projecto/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/projecto/navigation/AppNavigation.kt
@@ -1,1 +1,2 @@
 //Aca se define la navegacion entre las pantallas. Se agregan las rutas y se asocian.
+
--- app/src/main/java/com/example/projecto/ui/screens/RegisterScreen.kt
+++ app/src/main/java/com/example/projecto/ui/screens/RegisterScreen.kt
@@ -1,1 +1,89 @@
 //Diseño y logica para que nuevo usuario cree una cuenta.
+
+package com.example.proyecto.ui.screens
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import androidx.compose.material.icons.Icons
+//import androidx.compose.material.icons.filled.Visibility
+//import androidx.compose.material.icons.filled.VisibilityOff
+
+
+@Composable
+fun RegisterScreen(navController: NavController) {
+    //Estado para guardar lo que escribe el usuario.
+    var nombre by remember { mutableStateOf("") }
+    var email by remember { mutableStateOf( "") }
+    var password by remember { mutableStateOf("") }
+    var passwordTwo by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "Registro",
+            style = MaterialTheme.typography.headlineMedium
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        TextField(
+            value = nombre,
+            onValueChange = {nombre = it },
+            label = { Text("Nombre") },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+
+        TextField(
+            value = email,
+            onValueChange = {email = it },
+            label = { Text("Email") },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+
+        TextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Contraseña") },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        TextField(
+            value = passwordTwo,
+            onValueChange = { passwordTwo = it },
+            label = { Text("Repetir contraseña") },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Button(
+            onClick = {
+                //Aca va la logica.
+            },
+            modifier = Modifier.fillMaxWidth()
+        ){
+            Text("Registrarme")
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun RegisterScreenPreview(){
+    RegisterScreen(navController = rememberNavController())
+}
