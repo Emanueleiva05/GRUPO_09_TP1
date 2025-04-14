28ced11c89fef6f4543c15885aa9e9cae16e4169
Emanuel Leiva <emanueleiva12@gmail.com>
2025-04-14 15:30:27 -0300

    Se agrego la funcionalidad basica del inicio de sesion. Por ahora anda pero con una lista de usuarios hardcodeada

--- app/src/main/java/com/example/projecto/app/InicioSesionActivity.kt
+++ app/src/main/java/com/example/projecto/app/InicioSesionActivity.kt
@@ -7,6 +7,9 @@ import androidx.appcompat.widget.AppCompatButton
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.projecto.R
+import android.content.Intent
+import androidx.appcompat.widget.AppCompatEditText
+import android.widget.Toast
 
 class InicioSesionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +20,34 @@ class InicioSesionActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val btnlog = findViewById<AppCompatButton>(R.id.btnlog)
+        val name = findViewById<AppCompatEditText>(R.id.etUser)
+        val pass = findViewById<AppCompatEditText>(R.id.etPassword)
+
+        val users: List<List<String>> = listOf(
+            listOf("Juan torres"," " ,"123456"),
+            listOf("Emanuel Leiva"," " ,"123"),
+            listOf("Tomas battistella"," " , "2345")
+        )
+
+        btnlog.setOnClickListener {
+            val nameText = name.text.toString()
+            val passText = pass.text.toString()
+            var encontrado: Boolean = false
+
+            for (user in users){
+                if(user[0] == nameText && user[2]==passText){
+                    val intent = Intent(this, HomeActivity::class.java)
+                    startActivity(intent)
+                    break
+                }
+            }
+
+            if(!encontrado){
+                Toast.makeText(this, "Usuario no encontrado", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
