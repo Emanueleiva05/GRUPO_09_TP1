a6e6ad5a99c51ad6dc867013a464e6114093c6ea
Irineo Hiriart <irineohiriart@alu.frlp.utn.edu.ar>
2025-04-23 16:02:37 -0300

    Login - Agregue validaciones. Pantalla terminada.

--- app/src/main/java/com/example/projecto/ui/theme/Color.kt
+++ app/src/main/java/com/example/projecto/ui/theme/Color.kt
@@ -3,7 +3,7 @@ package com.example.projecto.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val Fondo = Color(0xFF000000)
-val Primario = Color(0xFF21D223)
-val Secundario = Color(0xFF607D8B)
+val Primario = Color(0xFFB4B4B4)
+val Secundario = Color(0xFFADCED3)
 val Texto = Color(0xFFFFFFFF)
 val TextoSecundario = Color(0xFFA6A6A6)
